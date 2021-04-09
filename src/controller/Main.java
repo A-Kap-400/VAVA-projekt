ca5d09dc391b377870e2b4c1f2ca4151f900e047
A-Kap-400 <kappelakos@gmail.com>
2021-04-09 09:23:08 +0200

    initial commit

---
+++ src/controller/Main.java
@@ -0,0 +1,22 @@
+package controller;
+
+/**
+ * Tento projekt vznikol vramci predmetu Vyvoj aplikacii s viacvrstvovou
+ * architekturou na Fakulte informatiky a informacnych technologii na Slovenskej
+ * technickej univerzite.
+ *
+ * @author Akos Kappel
+ * @author Patrik Velcicky
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+    }
+
+}
